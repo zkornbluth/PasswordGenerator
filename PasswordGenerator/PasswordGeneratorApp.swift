6e9f6ea4806ae1feda60d8deb3ee72d7b9aacf7c
zkornbluth <zkornbluth2007@gmail.com>
2025-07-29 16:47:14 -0400

    Change to menu bar app
    Replaced WindowGroup with MenuBarExtra, hid app in dock, added white outline icon in keeping with other menu bar icons.

--- PasswordGenerator/PasswordGeneratorApp.swift
+++ PasswordGenerator/PasswordGeneratorApp.swift
@@ -10,11 +10,35 @@ import SwiftUI
 @main
 struct PasswordGeneratorApp: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .navigationTitle("Password Generator")
-                .frame(width: 300, height: 300)
+        MenuBarExtra {
+                    ContentView()
+                        .frame(width: 300, height: 300)
+                        .overlay(alignment: .topTrailing) {
+                                    Button(
+                                        "Quit",
+                                        systemImage: "xmark.circle.fill"
+                                    ) {
+                                        NSApp.terminate(nil)
+                                    }
+                                    .labelStyle(.iconOnly)
+                                    .buttonStyle(.plain)
+                                    .padding(6)
+                                }
+                                .frame(width: 300, height: 300)
+        } label: {
+            Label {
+                    Text("Password Generator")
+                } icon: {
+                    let image: NSImage = {
+                        let ratio = $0.size.height / $0.size.width
+                        $0.size.height = 18
+                        $0.size.width = 18 / ratio
+                        return $0
+                    }(NSImage(named: "MenuBarIcon")!)
+                    
+                    Image(nsImage: image)
+                }
         }
-        .defaultSize(CGSize(width: 300, height: 300))
+        .menuBarExtraStyle(.window)
     }
 }
